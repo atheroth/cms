9cb1b55538043d41bc37a637c4bf222cf25b45d4
Алексей <110392375+atheroth@users.noreply.github.com>
2024-07-14 18:19:24 +0300

    adding view create post

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.main')
-@section('title', 'Posts')
 @section('content')
+    <form action="">
+        <div class="mb-2">
+            <h5><label class="form-label mt-2">Заголовок</label></h5>
+            <input type="text" class="form-control" id="title" placeholder="Заголовок статьи">
+        </div>
+        <div class="mb-2">
+            <h5><label class="form-label">Содержание статьи</label></h5>
+            <textarea class="form-control" id="content" rows="3" placeholder="Содержание статьи"></textarea>
+        </div>
 
+        <div class="mb-2">
+            <h5><label class="form-label">Изображение</label></h5>
+            <input type="text" class="form-control" id="image" placeholder="Ссылка на картинку">
+        </div>
+
+        <div class="mb-2">
+            <h5><label class="form-label">Лайки</label></h5>
+            <input type="text" class="form-control" id="likes" placeholder="Количество лайков">
+        </div>
+
+        <button type="submit" class="btn btn-success mt-2">Создать</button>
+    </form>
 @endsection
