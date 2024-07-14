06c65c48a1dc5916a36003521c786c7514b4f5a8
Алексей <110392375+atheroth@users.noreply.github.com>
2024-07-14 18:25:06 +0300

    adding names input fields

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -4,21 +4,21 @@
         @csrf
         <div class="mb-2">
             <h5><label class="form-label mt-2">Заголовок</label></h5>
-            <input type="text" class="form-control" id="title" placeholder="Заголовок статьи">
+            <input type="text" name="title" class="form-control" id="title" placeholder="Заголовок статьи">
         </div>
         <div class="mb-2">
             <h5><label class="form-label">Содержание статьи</label></h5>
-            <textarea class="form-control" id="content" rows="3" placeholder="Содержание статьи"></textarea>
+            <textarea name="content" class="form-control" id="content" rows="3" placeholder="Содержание статьи"></textarea>
         </div>
 
         <div class="mb-2">
             <h5><label class="form-label">Изображение</label></h5>
-            <input type="text" class="form-control" id="image" placeholder="Ссылка на картинку">
+            <input type="text" name="image" class="form-control" id="image" placeholder="Ссылка на картинку">
         </div>
 
         <div class="mb-2">
             <h5><label class="form-label">Лайки</label></h5>
-            <input type="text" class="form-control" id="likes" placeholder="Количество лайков">
+            <input type="text" name="likes" class="form-control" id="likes" placeholder="Количество лайков">
         </div>
 
         <button type="submit" class="btn btn-success mt-2">Создать</button>
